2f2613cc060adf4b9911102402564467e8736fd4
Victor-Castro <victor.castro@mrjaffesclass.com>
2023-01-02 19:27:30 -0800

    finishit

---
+++ PDA/PDA.java
@@ -0,0 +1,34 @@
+/**
+ * Permissible Dating Age program
+ *
+ * @author Mr. Jaffe
+ * @version 2021-06-22 Version 1.0.0
+ */
+public class PDA
+{
+
+    /**
+     * Constructor for objects of class PDA
+     */
+    public PDA()
+    {
+        // We don't need to do anything in the constructor for
+        // our program.
+    }
+    
+    /**
+     * This is the main event loop for our PDA program
+     */
+    public void runEventLoop() {
+        System.out.println("PDA!");        
+    }
+
+    /**
+     * The main method instantiates and runs the program
+     */
+    public static void main (String[] args) {
+        PDA pda = new PDA();
+        pda.runEventLoop();
+    }
+}
+
